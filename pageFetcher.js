3842ad4f002917d1e4624328726194a319016abb
djtwdix <djtwd.ix@icloud.com>
2021-02-10 21:22:04 -0400

    Implemented if file doesn't exist write file

--- pageFetcher.js
+++ pageFetcher.js
@@ -1,7 +1,7 @@
 const args = process.argv.slice(2);
 const request = require('request');
 const fs = require('fs');
-const { Z_FIXED } = require('zlib');
+//const { Z_FIXED } = require('zlib');
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -15,7 +15,15 @@ request(args[0], (error, response, body) => {
     process.exit();
   }
   fs.stat(args[1], (err, stats) => {
-    if (err) throw err
+    if (err) {
+      fs.writeFile(args[1], body, (err) => {
+        if (err) {
+          console.log("Local path file was invalid")
+        }
+        console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`)
+        rl.close();
+      });
+    }
     if (stats !== undefined) {
       rl.question(`${args[1]} already exists, type Y then enter to overwrite the file or N and then enter to cancel \n`, answer => {
         if (answer === 'Y' || answer === 'y') {
