4e6c7bb84521ea725c0d8c74fcb923aee3c1b8b3
djtwdix <djtwd.ix@icloud.com>
2021-02-10 20:41:03 -0400

    Implemented request based on args and write file, haven't figured out how to get the bytes back yet

---
+++ pageFetcher.js
@@ -0,0 +1,13 @@
+const args = process.argv.slice(2);
+const request = require('request');
+const fs = require('fs');
+
+request(args[0], (error, response, body) => {
+  console.log("error: ", error)
+  console.log('statusCode:', response && response.statusCode); 
+  fs.writeFile(args[1], body, (err) => {
+    if (err) throw err
+    console.log(`Downloaded and saved ${bytes} to ${args[1]}`)
+  });
+})
+
