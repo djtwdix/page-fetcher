6e0f53df0ba0274218a8e85950ba11c841dd4c1f
djtwdix <djtwd.ix@icloud.com>
2021-02-10 21:15:26 -0400

    Update

--- pageFetcher.js
+++ pageFetcher.js
@@ -1,13 +1,37 @@
 const args = process.argv.slice(2);
 const request = require('request');
 const fs = require('fs');
+const { Z_FIXED } = require('zlib');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 request(args[0], (error, response, body) => {
-  console.log("error: ", error)
-  console.log('statusCode:', response && response.statusCode); 
-  fs.writeFile(args[1], body, (err) => {
+  if (error) {
+    console.log("There was an area retrieving data from the provided URL")
+    process.exit();
+  }
+  fs.stat(args[1], (err, stats) => {
     if (err) throw err
-    console.log(`Downloaded and saved bytes to ${args[1]}`)
-  });
+    if (stats !== undefined) {
+      rl.question(`${args[1]} already exists, type Y then enter to overwrite the file or N and then enter to cancel \n`, answer => {
+        if (answer === 'Y' || answer === 'y') {
+          rl.close();
+          fs.writeFile(args[1], body, (err) => {
+            if (err) {
+              console.log("Local path file was invalid")
+            }
+            console.log(`Downloaded and saved ${body.length} bytes to ${args[1]}`)
+          });
+        }
+        if (answer === 'N' || answer === 'n') {
+          process.exit();
+        }
+      })
+    }
+  })
 })
 
